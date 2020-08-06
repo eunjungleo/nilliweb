ce88eb5ddfc74cabc9f0859702c766a731cc4ef9
eunjungleo <asaprocky123@likelion.org>
2020-08-06 21:32:32 +0900

    create models.py

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Content(models.Model):
+    title = models.TextField()
+    url = models.URLField()
+    date = models.DateTimeField(auto_now=True)
+    korean = models.TextField(null=True)
+    chinese = models.TextField(null=True)
+    turkmen = models.TextField(null=True)
+    eng = models.TextField(null=True)
+
+    def __str__(self):
+        return str(self.pk) + "/ " + self.title
+    
+
