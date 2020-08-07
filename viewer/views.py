9d12057ca9219b153fdd15c836584456c55b5ac6
eunjungleo <asaprocky123@likelion.org>
2020-08-08 02:12:51 +0900

    feature YOUTUBE player on detail.html

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import get_object_or_404
 
 
 class ContentsAll(ListView):
@@ -8,10 +9,11 @@ class ContentsAll(ListView):
     template_name = "list.html"
 
 
-class ContentDetail(DetailView):
-    model = Content
-    template_name = "detail.html"
-
 class AboutView(TemplateView):
     template_name = "about.html"
 
+# detailview
+def detail_view(requests, pk):
+    obj = get_object_or_404(Content, pk=pk)
+    youtube_id = obj.url.split("/", 3)[3]
+    return render(requests, "detail.html", {'youtube_id':youtube_id, 'obj':obj})
