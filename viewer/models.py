a25944ac9d6fb6b90e0640681a233303ef896bb5
eunjungleo <asaprocky123@likelion.org>
2020-08-10 12:45:41 +0900

    add question fields to Content

--- viewer/models.py
+++ viewer/models.py
@@ -4,10 +4,18 @@ class Content(models.Model):
     title = models.TextField()
     url = models.URLField()
     date = models.DateTimeField(auto_now=True)
+
     korean = models.TextField(null=True)
     chinese = models.TextField(null=True)
     turkmen = models.TextField(null=True)
     eng = models.TextField(null=True)
+
+    q1 = models.IntegerField(null=True)
+    q2 = models.IntegerField(null=True)
+    q3 = models.IntegerField(null=True)
+    q4 = models.IntegerField(null=True)
+    q5 = models.IntegerField(null=True)
+
 
     def __str__(self):
         return str(self.pk) + "/ " + self.title
